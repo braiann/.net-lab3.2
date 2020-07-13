b3a169f48eb57301c7db0f7682252566170cd393
unknown <villasantibraian@icloud.com>
2020-07-13 00:47:11 -0300

    terminado lab grilla

---
+++ LabGrilla/formListaUsuarios.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LabGrilla
+{
+    public partial class formListaUsuarios : Form
+    {
+        public Negocio.Usuarios usuarios;
+        public Negocio.Usuarios OUsuarios
+        {
+            get => usuarios;
+            set => usuarios = value;
+        }
+
+        public formListaUsuarios()
+        {
+            InitializeComponent();
+            this.dgvUsuarios.AutoGenerateColumns = false;
+            this.GenerarColumnas();
+            this.OUsuarios = new Negocio.Usuarios();
+            this.dgvUsuarios.DataSource = this.OUsuarios.GetAll();
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            this.GuardarCambios();
+            this.RecargarGrilla();
+        }
+
+        private void RecargarGrilla()
+        {
+            this.dgvUsuarios.DataSource = this.OUsuarios.GetAll();
+        }
+
+        private void GuardarCambios()
+        {
+            this.OUsuarios.GuardarCambios((DataTable)this.dgvUsuarios.DataSource);
+        }
+
+        private void GenerarColumnas()
+        {
+            DataGridViewTextBoxColumn colNroDoc = new DataGridViewTextBoxColumn();
+            colNroDoc.Name = "nro_doc";
+            colNroDoc.HeaderText = "Número de documento";
+            colNroDoc.DataPropertyName = "nro_doc";
+            colNroDoc.DisplayIndex = 0;
+            colNroDoc.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
+
+            DataGridViewComboBoxColumn colTipoDoc = new DataGridViewComboBoxColumn();
+            colTipoDoc.Name = "tipo_doc";
+            colTipoDoc.HeaderText = "Tipo de documento";
+            colTipoDoc.DataPropertyName = "tipo_doc";
+            colTipoDoc.DisplayIndex = 0;
+            colTipoDoc.DataSource = this.getTiposDocumento();
+            colTipoDoc.ValueMember = "cod_tipo_doc";
+            colTipoDoc.DisplayMember = "desc_tipo_doc";
+
+            DataGridViewTextBoxColumn colTel = new DataGridViewTextBoxColumn();
+            colTel.Name = "telefono";
+            colTel.HeaderText = "Teléfono";
+            colTel.DataPropertyName = "telefono";
+            
+            DataGridViewTextBoxColumn colEmail = new DataGridViewTextBoxColumn();
+            colEmail.Name = "email";
+            colEmail.HeaderText = "Email";
+            colEmail.DataPropertyName = "email";
+            colEmail.Width = 250;
+
+            DataGridViewTextBoxColumn colCel = new DataGridViewTextBoxColumn();
+            colCel.Name = "celular";
+            colCel.HeaderText = "Celular";
+            colCel.DataPropertyName = "celular";
+
+            DataGridViewTextBoxColumn colUsuario = new DataGridViewTextBoxColumn();
+            colUsuario.Name = "usuario";
+            colUsuario.HeaderText = "Usuario";
+            colUsuario.DataPropertyName = "usuario";
+
+            DataGridViewTextBoxColumn colClave = new DataGridViewTextBoxColumn();
+            colClave.Name = "clave";
+            colClave.HeaderText = "Clave";
+            colClave.DataPropertyName = "clave";
+            colClave.Visible = false;
+
+            this.dgvUsuarios.Columns["direccion"].Width = 250;
+            this.dgvUsuarios.Columns["apellido"].DefaultCellStyle.Font = new Font(this.dgvUsuarios.DefaultCellStyle.Font, FontStyle.Bold);
+            this.dgvUsuarios.Columns["nombre"].DefaultCellStyle.Font = new Font(this.dgvUsuarios.DefaultCellStyle.Font, FontStyle.Bold);
+            this.dgvUsuarios.Columns["fecha_nac"].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
+
+            this.dgvUsuarios.Columns.Add(colNroDoc);
+            this.dgvUsuarios.Columns.Add(colTipoDoc);
+            this.dgvUsuarios.Columns.Add(colTel);
+            this.dgvUsuarios.Columns.Add(colEmail);
+            this.dgvUsuarios.Columns.Add(colCel);
+            this.dgvUsuarios.Columns.Add(colUsuario);
+            this.dgvUsuarios.Columns.Add(colClave);
+        }
+
+        private DataTable getTiposDocumento()
+        {
+            DataTable dtTiposDoc = new DataTable();
+
+            dtTiposDoc.Columns.Add("cod_tipo_doc", typeof(int));
+            dtTiposDoc.Columns.Add("desc_tipo_doc", typeof(string));
+
+            dtTiposDoc.Rows.Add(new object[] { 1, "DNI" });
+            dtTiposDoc.Rows.Add(new object[] { 2, "Cédula" });
+            dtTiposDoc.Rows.Add(new object[] { 3, "Pasaporte" });
+            dtTiposDoc.Rows.Add(new object[] { 4, "Libreta Cívica" });
+            dtTiposDoc.Rows.Add(new object[] { 5, "Libreta Enrolamiento" });
+
+            return dtTiposDoc;
+        }
+    }
+}
